800244b8cd05fdfa02f30b9ee874c6fa6df3aa71
Barbra H <barbrahoudeville@gmail.com>
2018-05-27 10:43:42 +0200

    ajout du projet

---
+++ CasStagiairesDLLBase/classesMetierStagiaires/ConnexionBDD.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+
+namespace classesMetierStagiaires
+{
+    public class ConnexionBDD
+    {
+        private MySqlConnection connection;
+        private string server;
+        private string database;
+        private string uid;
+        private string password;
+
+        //Constructor
+        public ConnexionBDD()
+        {
+            Initialize();
+        }
+
+        //Initialize values
+        private void Initialize()
+        {
+            server = "localhost";
+            database = "bddthierry";
+            uid = "root";
+            password = "";
+            string connectionString;
+            connectionString = "SERVER=" + server + ";" + "DATABASE=" +
+            database + ";" + "UID=" + uid + ";" + "PASSWORD=" + password + ";";
+
+            connection = new MySqlConnection(connectionString);
+        }
+
+        private bool OpenConnection()
+        {
+            try
+            {
+                connection.Open();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                //When handling errors, you can your application's response based 
+                //on the error number.
+                //The two most common error numbers when connecting are as follows:
+                //0: Cannot connect to server.
+                //1045: Invalid user name and/or password.
+                switch (ex.Number)
+                {
+                    case 0:
+                        MessageBox.Show("Cannot connect to server.  Contact administrator");
+                        break;
+
+                    case 1045:
+                        MessageBox.Show("Invalid username/password, please try again");
+                        break;
+                }
+                return false;
+            }
+        }
+
+        //Close connection
+        private bool CloseConnection()
+        {
+            try
+            {
+                connection.Close();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+                return false;
+            }
+        }
+
+        /// <summary>
+        ///  Fonction qui execute des requetes en base, sur la connexion active
+        /// </summary>
+        /// <param name="query"> la requete à executer </param>
+        /// <returns></returns>
+        public int executeQuery(string query)
+        {
+
+
+            //open connection
+            if (this.OpenConnection() == true)
+            {
+                //create command and assign the query and connection from the constructor
+                MySqlCommand cmd = new MySqlCommand(query, this.connection);
+
+                //Execute command
+                return cmd.ExecuteNonQuery();
+            }
+            return 0;
+        }
+
+    }
+}
---
+++ CasStagiairesDLLBase/classesMetierStagiaires/MStagiaireDE.cs
@@ -0,0 +1,82 @@
+using System;
+
+
+namespace classesMetierStagiaires
+{
+    /// <summary>
+    /// stagiaire spécialisé statut Demandeur d'emploi
+    /// </summary>
+    public class MStagiaireDE : MStagiaire
+    {
+
+        /// <summary>
+        /// constructeur d'initialisation
+        /// (appelle le constructeur d'initialisation de la classe de base)
+        /// </summary>
+        /// <param name="unNumosia">numéro OSIA</param>
+        /// <param name="unNom">Nom</param>
+        /// <param name="unPrenom">Prénom</param>
+        /// <param name="uneRue">Adresse</param>
+        /// <param name="uneVille">Ville</param>
+        /// <param name="UnCodePostal">Code Postal</param>
+        /// <param name="unRemuAfpa">Rémunaration par l'Afpa</param>
+
+        // constante qui indique le fait qu'il s'agit d'un objet StagiaireDE
+        protected const Boolean DE = true;
+
+        // relation avec la table section
+        protected String codeSection;
+        
+        public MStagiaireDE(Int32 unNumosia, String unNom, String unPrenom, String uneRue, String uneVille, String UnCodePostal,
+            Boolean unRemuAfpa)
+            : base( unNumosia,  unNom,  unPrenom,  uneRue,  uneVille,  UnCodePostal)
+        {
+            this.RemuAfpaStagiaire = unRemuAfpa;
+        }
+
+        /// <summary>
+        /// Rémuneration par l'Afpa
+        /// </summary>
+        private Boolean remuAfpaStagiaire;
+
+        /// <summary>
+        /// obtient ou définit Rémunération par l'Afpa
+        /// </summary>
+        public Boolean RemuAfpaStagiaire
+        {
+            get
+            {
+                return this.remuAfpaStagiaire;
+            }
+            set
+            {
+                this.remuAfpaStagiaire = value;
+            }
+        }
+
+        public String CodeSection {
+            get { return this.codeSection; }
+            set {
+                this.codeSection = value;
+            }
+        }
+
+        // ecrit la requete en bdd à faire executer par l'object de connexion
+        public bool Save(ConnexionBDD connexion)
+        {
+            //INSERT INTO
+            string query = "INSERT INTO stagiaire (id, numOsia, prenom, nom, rue, ville, cp, de, remuafpa, idsectionsuivie) VALUES ('','" +
+                this.numOsiaStagiaire + "', '" + this.PrenomStagiaire + "','" + this.NomStagiaire + "','" +
+                this.RueStagiaire + "','" + this.VilleStagiaire + "','" + this.CodePostalStagiaire +
+                "','" + DE + "','" + this.RemuAfpaStagiaire + ", '" + this.codeSection + "')";
+
+            int result = connexion.executeQuery(query);
+
+            if (result == 1) { return true; }
+
+            // mieux : lever une exception
+            return false;
+        }
+
+    }
+}
